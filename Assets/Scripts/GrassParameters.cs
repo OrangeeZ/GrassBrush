ea582e01415493534e2e34a6b9708a4096898918
OrangeeZ <ultramegacrasher@gmail.com>
2016-07-11 09:19:14 +0300

    ~ Initial commit

---
+++ Assets/Scripts/GrassParameters.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Grass
+{
+	public class GrassParameters : MonoBehaviour
+	{
+
+		[Header ("Size")]
+
+		[SerializeField]
+		private float _minGrassSize = 0.7f;
+
+		[SerializeField]
+		private float _maxGrassSize = 1.3f;
+
+		[Header ("Distribution")]
+
+		[SerializeField]
+		private int _seed;
+
+		[SerializeField]
+		private float _maxOffset = 0.1f;
+
+		[ContextMenu("Generate seed")]
+		public void GenerateSeed ()
+		{
+			_seed = Mathf.FloorToInt(Random.value * int.MaxValue);
+		}
+
+		public float GetScale (Vector3 worldPosition)
+		{
+			Random.seed = _seed;
+
+			var perlinOffset = Random.insideUnitCircle;
+			perlinOffset.x += worldPosition.x;
+			perlinOffset.y += worldPosition.z;
+
+			var perlinValue = Mathf.PerlinNoise (perlinOffset.x, perlinOffset.y);
+
+			return Mathf.Lerp(_minGrassSize, _maxGrassSize, perlinValue);
+		}
+
+		public Vector3 GetOffset (Vector3 worldPosition)
+		{
+			Random.seed = _seed;
+
+			var perlinOffset = Random.insideUnitCircle;
+			perlinOffset.x += worldPosition.x;
+			perlinOffset.y += worldPosition.z;
+
+			var perlinValue = Mathf.PerlinNoise (perlinOffset.x, perlinOffset.y);
+			var offset = Mathf.Lerp (-_maxOffset, _maxOffset, perlinValue);
+
+			var unitCircleOffsetDirection = Random.insideUnitCircle.normalized;
+			var result = new Vector3 (unitCircleOffsetDirection.x, 0, unitCircleOffsetDirection.y);
+
+			return result * offset;
+		}
+	}
+}
---
+++ Assets/Scripts/GrassQuadGridGenerator.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grass
+{
+	[RequireComponent(typeof(MeshFilter))]
+	[RequireComponent(typeof(MeshRenderer))]
+	public class GrassQuadGridGenerator : MonoBehaviour
+	{
+		[SerializeField]
+		private Mesh _gridMesh;
+
+		[SerializeField]
+		private Mesh _quadMesh;
+
+		private List<Vector3> _vertexBuffer;
+		private List<Vector3> _normalBuffer;
+		private List<Vector2> _uvBuffer;
+		private List<int> _triangleBuffer;
+
+		[SerializeField]
+		private Mesh _resultMesh;
+
+		[SerializeField]
+		private GrassParameters _parameters;
+
+		[ContextMenu("Generate")]
+		private void Generate()
+		{
+			_vertexBuffer = new List<Vector3>();
+			_normalBuffer = new List<Vector3>();
+			_uvBuffer = new List<Vector2>();
+			_triangleBuffer = new List<int>();
+
+			_resultMesh = new Mesh();
+			_resultMesh.MarkDynamic();
+
+			var gridVertices = _gridMesh.vertices;
+			foreach (var each in gridVertices)
+			{
+				AddMeshToBuffers(each, _quadMesh);
+			}
+
+			_resultMesh.SetVertices(_vertexBuffer);
+			_resultMesh.SetNormals(_normalBuffer);
+			_resultMesh.SetUVs(0, _uvBuffer);
+			_resultMesh.SetTriangles(_triangleBuffer, 0);
+
+			GetComponent<MeshFilter> ().sharedMesh = _resultMesh;
+		}
+
+		private void AddMeshToBuffers(Vector3 centerPosition, Mesh mesh)
+		{
+			var worldPosition = transform.localToWorldMatrix.MultiplyPoint3x4 (centerPosition);
+			var scale = _parameters.GetScale (worldPosition);
+			var offset = _parameters.GetOffset (worldPosition);
+
+			_triangleBuffer.AddRange(mesh.triangles.Select(_ => _ + _vertexBuffer.Count).ToArray());
+
+			_vertexBuffer.AddRange(mesh.vertices.Select(_ => _ * scale + offset + centerPosition).ToArray());
+			_normalBuffer.AddRange(mesh.normals);
+			_uvBuffer.AddRange(mesh.uv);
+		}
+	}
+}
