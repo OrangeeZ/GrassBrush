04d490a3abbac960a79bad429a834c1b69a8f643
ivan.kozlovskih <ivan.kozlovskih@nival.com>
2016-08-19 15:29:25 +0300

    + DetailObjectData class

---
+++ Assets/Scripts/DetailObjectsData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class DetailObjectsData : ScriptableObject
+{
+    [SerializeField]
+    private List<DistributedCircleGenerator.Circle> _detailObjects;
+
+    public IList<DistributedCircleGenerator.Circle> GetDetailObjects()
+    {
+        return _detailObjects;
+    }
+
+    public void SetDetailObjects(IList<DistributedCircleGenerator.Circle> detailObjects)
+    {
+        _detailObjects = detailObjects.Select<DistributedCircleGenerator.Circle, DistributedCircleGenerator.Circle>(InstanceSelector).ToList();
+    }
+
+    private DistributedCircleGenerator.Circle InstanceSelector(DistributedCircleGenerator.Circle _)
+    {
+        var result = new DistributedCircleGenerator.Circle
+                   {
+                       Position = _.Position,
+                       Radius = _.Radius,
+                       Instance = UnityEditor.PrefabUtility.GetPrefabParent(_.Instance) as DetailPreset
+                   };
+
+        if (result.Instance == null)
+        {
+            Debug.LogError("null instance!");
+        }
+
+        return result;
+    }
+}
